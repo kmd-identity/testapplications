fd765b9f50463ed90770da606f82ed34c161dbdb
zkokmddk <ZKO@kmd.dk>
2021-06-07 13:25:53 +0200

    Updated appsettings and changed how API Url is retrieved

--- KMD.Identity.TestApplications.OpenID.MVCCore/Controllers/HomeController.cs
+++ KMD.Identity.TestApplications.OpenID.MVCCore/Controllers/HomeController.cs
@@ -12,16 +12,19 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace KMD.Identity.TestApplications.OpenID.MVCCore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public IConfiguration Configuration { get; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         public IActionResult Index()
@@ -40,7 +43,7 @@ namespace KMD.Identity.TestApplications.OpenID.MVCCore.Controllers
                 // Retrieving access_token from session because that's how we stored it in OnTokenResponseReceived in Startup.cs
                 var rawAccessToken = HttpContext.Session.GetString("access_token");
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", rawAccessToken);
-                var response = await httpClient.GetAsync("https://localhost:44377/api/claims");
+                var response = await httpClient.GetAsync(Configuration["Security:ApiUrl"]);
                 var apiCallResult = await response.Content.ReadAsStringAsync();
 
                 ViewBag.Result = apiCallResult;
