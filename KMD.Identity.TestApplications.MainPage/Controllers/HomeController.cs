50716dd60826cec18dda3a32d55bef8ca097a57b
zkokmddk <ZKO@kmd.dk>
2021-06-08 13:52:28 +0200

    Updated mainpage to retrieve URL's for testapplications based on appsettings

---
+++ KMD.Identity.TestApplications.MainPage/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using KMD.Identity.TestApplications.MainPage.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace KMD.Identity.TestApplications.MainPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public IConfiguration Configuration { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.SAMLBaseUrl = Configuration["SAML:BaseUrl"];
+            ViewBag.OpenIDWebBaseUrl = Configuration["OpenIDWeb:BaseUrl"];
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ KMD.Identity.TestApplications.MainPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KMD.Identity.TestApplications.MainPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
