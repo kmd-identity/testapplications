e171957768ed2c4ee96c0bd48ea6c31628540c8d
Jamie Robson (JPL) <jamierobson@users.noreply.github.com>
2022-08-16 10:05:13 +0200

    Add basic test page for delegation, to hit the "dumb" endpoint

--- KMD.Identity.TestApplications.OpenID.Angular/ClientApp/src/app/app.module.ts
+++ KMD.Identity.TestApplications.OpenID.Angular/ClientApp/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AuthConfigModule } from './auth/auth-config.module';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { AppConfig } from './config/app.config';
+import { UserDelegationComponent } from './user-delegation/user-delegation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDelegationComponent
   ],
   imports: [
     BrowserModule,
