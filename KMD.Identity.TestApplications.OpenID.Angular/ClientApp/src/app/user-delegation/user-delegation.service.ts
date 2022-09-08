cd86d11c71dc4c44789ad43363a07e23adde5221
Jamie Robson (JPL) <jamierobson@users.noreply.github.com>
2022-09-08 14:55:14 +0200

    Initiate the delegation flow specifying if we should, or should not, have the stub consents endpoint return a consent

--- KMD.Identity.TestApplications.OpenID.Angular/ClientApp/src/app/user-delegation/user-delegation.service.ts
+++ KMD.Identity.TestApplications.OpenID.Angular/ClientApp/src/app/user-delegation/user-delegation.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { OidcSecurityService, AuthOptions } from 'angular-auth-oidc-client';
 import { BehaviorSubject } from 'rxjs';
 import { AuthenticationContext } from '../authenticate/authentication-context.service';
-import { ConfigIds, IdentityProviders } from '../config/auth-config.module';
+import { ConfigIds } from '../config/auth-config.module';
 
 @Injectable({
   providedIn: 'root'
@@ -22,13 +22,14 @@ export class UserDelegationService {
       })
   }
 
-  public requestTokenExchange() {
+  public requestTokenExchange(myDomain: string, actAsSubjectDomain: string, consentGrantedMasquerade: boolean) {
 
     const authOption: AuthOptions = {
       customParams: {
       "userdelegation_actas_nameid": "myself-for-now-until-implementation-is-provided",
-      "userdelegation_actas_identityprovider": IdentityProviders.KmdAd,
-      "domain_hint": IdentityProviders.KmdAd,
+      "userdelegation_actas_identityprovider": actAsSubjectDomain,
+      "domain_hint": myDomain,
+      "userdelegation_endpoint_should_reply_with_stub_consent": consentGrantedMasquerade // keep in mind that this is a developer convenience while we deliver the feature. It WILL disappear
     }};
     
     this.oidcSecurityService.authorize(ConfigIds.UserDelegation, authOption);
