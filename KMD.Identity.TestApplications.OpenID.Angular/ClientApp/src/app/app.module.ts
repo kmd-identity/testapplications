93c3dfc6af869873d76b5a033a6af20f0fc0ef43
Julie Sindhu Jensen <JLJ@kmd.dk>
2023-04-14 09:24:51 +0200

    Removing user delegation

--- KMD.Identity.TestApplications.OpenID.Angular/ClientApp/src/app/app.module.ts
+++ KMD.Identity.TestApplications.OpenID.Angular/ClientApp/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AuthConfigModule } from './config/auth-config.module';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { AppConfig } from './config/app.config';
-import { UserDelegationComponent } from './user-delegation/user-delegation.component';
 import { ErrorComponent } from './error/error.component';
 import { UserClaimsComponent } from './user-claims/user-claims.component';
 import { TestApiCallComponent } from './test-api-call/test-api-call.component';
@@ -15,7 +14,6 @@ import { AuthenticateComponent } from './authenticate/authenticate.component';
 @NgModule({
   declarations: [
     AppComponent,
-    UserDelegationComponent,
     ErrorComponent,
     UserClaimsComponent,
     TestApiCallComponent,
