0058b63293735a7759a84415aa15588bd17f8aa2
Morten Christiansen <mc@uvdata.dk>
2023-09-01 13:32:53 +0200

    Removed net5.0 incompatible c# syntax

--- KMD.Identity.TestApplications.SAML.MVCCore/Config/ExtendedSaml2Configuration.cs
+++ KMD.Identity.TestApplications.SAML.MVCCore/Config/ExtendedSaml2Configuration.cs
@@ -1,11 +1,12 @@
 ﻿using ITfoxtec.Identity.Saml2;
 
-namespace KMD.Identity.TestApplications.SAML.MVCCore.Config;
-
-public class ExtendedSaml2Configuration : Saml2Configuration
+namespace KMD.Identity.TestApplications.SAML.MVCCore.Config
 {
-    public string IdPMetadata { get; set; }
-    public string SigningCertificateThumbprint { get; set; }
-    public string SigningCertificateFile { get; set; }
-    public string SigningCertificatePassword { get; set; }
+    public class ExtendedSaml2Configuration : Saml2Configuration
+    {
+        public string IdPMetadata { get; set; }
+        public string SigningCertificateThumbprint { get; set; }
+        public string SigningCertificateFile { get; set; }
+        public string SigningCertificatePassword { get; set; }
+    }
 }
--- KMD.Identity.TestApplications.SAML.MVCCore/Extensions/Saml2ConfigurationExtensions.cs
+++ KMD.Identity.TestApplications.SAML.MVCCore/Extensions/Saml2ConfigurationExtensions.cs
@@ -3,28 +3,29 @@ using ITfoxtec.Identity.Saml2.Schemas.Metadata;
 using System;
 using System.Linq;
 
-namespace KMD.Identity.TestApplications.SAML.MVCCore.Extensions;
-
-public static class Saml2ConfigurationExtensions
+namespace KMD.Identity.TestApplications.SAML.MVCCore.Extensions
 {
-    public static void ReloadMetadata(this Saml2Configuration saml2Configuration, Uri metadataUrl)
+    public static class Saml2ConfigurationExtensions
     {
-        var entityDescriptor = new EntityDescriptor();
-        entityDescriptor.ReadIdPSsoDescriptorFromUrl(metadataUrl);
-        if (entityDescriptor.IdPSsoDescriptor != null)
+        public static void ReloadMetadata(this Saml2Configuration saml2Configuration, Uri metadataUrl)
         {
-            saml2Configuration.AllowedIssuer = entityDescriptor.EntityId;
-            saml2Configuration.SingleSignOnDestination = entityDescriptor.IdPSsoDescriptor.SingleSignOnServices.First().Location;
-            saml2Configuration.SingleLogoutDestination = entityDescriptor.IdPSsoDescriptor.SingleLogoutServices.First().Location;
-            saml2Configuration.SignatureValidationCertificates.AddRange(entityDescriptor.IdPSsoDescriptor.SigningCertificates);
-            if (entityDescriptor.IdPSsoDescriptor.WantAuthnRequestsSigned.HasValue)
+            var entityDescriptor = new EntityDescriptor();
+            entityDescriptor.ReadIdPSsoDescriptorFromUrl(metadataUrl);
+            if (entityDescriptor.IdPSsoDescriptor != null)
             {
-                saml2Configuration.SignAuthnRequest = entityDescriptor.IdPSsoDescriptor.WantAuthnRequestsSigned.Value;
+                saml2Configuration.AllowedIssuer = entityDescriptor.EntityId;
+                saml2Configuration.SingleSignOnDestination = entityDescriptor.IdPSsoDescriptor.SingleSignOnServices.First().Location;
+                saml2Configuration.SingleLogoutDestination = entityDescriptor.IdPSsoDescriptor.SingleLogoutServices.First().Location;
+                saml2Configuration.SignatureValidationCertificates.AddRange(entityDescriptor.IdPSsoDescriptor.SigningCertificates);
+                if (entityDescriptor.IdPSsoDescriptor.WantAuthnRequestsSigned.HasValue)
+                {
+                    saml2Configuration.SignAuthnRequest = entityDescriptor.IdPSsoDescriptor.WantAuthnRequestsSigned.Value;
+                }
             }
-        }
-        else
-        {
-            throw new Exception("IdPSsoDescriptor not loaded from metadata.");
+            else
+            {
+                throw new Exception("IdPSsoDescriptor not loaded from metadata.");
+            }
         }
     }
 }
