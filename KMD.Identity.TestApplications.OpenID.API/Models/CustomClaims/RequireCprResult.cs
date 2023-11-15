73f59304e811ce8f2618359ad815c87555a8ad31
Julie Sindhu Jensen <JLJ@kmd.dk>
2023-11-15 12:17:04 +0100

    Adding requirecprendpoint

---
+++ KMD.Identity.TestApplications.OpenID.API/Models/CustomClaims/RequireCprResult.cs
@@ -0,0 +1,7 @@
+﻿namespace KMD.Identity.TestApplications.OpenID.API.Models.CustomClaims
+{
+    public class RequireCprResult : ICustomClaimsResult
+    {
+        public string requirecpr { get; set; }
+    }
+}
