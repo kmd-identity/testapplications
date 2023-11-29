b263bddcb81bc2715282fe2fd4dc2bf6a9c04fd5
Wiszniewski.Grzegorz GPI <GPI@kmd.dk>
2023-11-29 13:35:03 +0100

    working android version

---
+++ KMD.Identity.TestApplications.OpenID.MAUI/Platforms/Android/AppSwitchActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace KMD.Identity.TestApplications.OpenID.MAUI;
+
+[Activity(Exported = true, Name = "kmd.identity.testapplication.maui.AppSwitchActivity")]
+public class AppSwitchActivity : Activity
+{
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        
+        SetResult(Result.Canceled);
+        Finish();
+    }
+}
